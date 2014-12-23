8d70821d02dcee403792cea93aee368e0fdeaf4b
Joe Bloggs <vapniks@yahoo.com>
2014-12-23 01:02:40 +0000

    update docs

--- gnus-summary-ext.el
+++ gnus-summary-ext.el
@@ -43,7 +43,7 @@
 ;;
 ;; This library provides extra limit commands for filtering the gnus summary buffer,
 ;; some commands for performing actions on MIME parts in articles, and some general
-;; functions for evaluating elisp code in all articles that have the process mark.
+;; functions for evaluating elisp code in marked articles.
 
 ;; You can apply complex filters for filtering the messages displayed in the *Summary* buffer
 ;; using `gnus-summary-ext-limit-filter', and you can save these filters in `gnus-summary-ext-saved-filters'.
